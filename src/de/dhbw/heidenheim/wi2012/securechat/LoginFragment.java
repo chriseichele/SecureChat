279848f1683e9d7f26f37dff25d73a2c2dfbca5b
chriseichele <youshallnotspam+github@gmail.com>
2014-07-25 13:52:28 +0200

    Selectable Text in Profile View and Chat Messages

--- src/de/dhbw/heidenheim/wi2012/securechat/LoginFragment.java
+++ src/de/dhbw/heidenheim/wi2012/securechat/LoginFragment.java
@@ -1,12 +1,10 @@
 package de.dhbw.heidenheim.wi2012.securechat;
 
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.

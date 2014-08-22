71561ff509790e3faef8459b55d21ea9a819c3fd
chriseichele <youshallnotspam+github@gmail.com>
2014-08-22 10:26:05 +0200

    RESTful Webservices Test

--- src/de/dhbw/heidenheim/wi2012/securechat/ServerConnector.java
+++ src/de/dhbw/heidenheim/wi2012/securechat/ServerConnector.java
@@ -2,6 +2,8 @@ package de.dhbw.heidenheim.wi2012.securechat;
 
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import com.sun.jersey.api.client.*;
+import javax.ws.rs.core.MediaType;
 
 import javax.crypto.KeyGenerator;
 
@@ -14,19 +16,21 @@ public class ServerConnector {
 	}
 	
 	public void dummy_connect() {
+		
 		//TODO It's just a Dummy
-		/*
 		
 		WebResource service = Client.create()
 									.resource( "wwi12-01.dhbw-heidenheim.de/SecureChat/webresources" );
 		System.out.println( service.path( "entities." )
-								   .accept( MediaType.TEXT_PLAIN)
+								   .accept( MediaType.APPLICATION_JSON)
 								   .get( String.class ) ); 
 		
-		*/
 	}
 	
 	public static Key getFileEncryptionKey() {
+		
+		//Testverindung aufbauen
+		new ServerConnector().dummy_connect();
 		
 		//TODO Get Key from Server instead of generating locally. This also solves current Problem with new Key on every new startup
 		if(key == null) {
